83f49b661cdba46d0defe30feab839cc234d4d08
00XAH00 <pxaxulin@gmail.com>
2023-02-18 12:41:26 +0300

    add user password hash and salt in database

--- BackEnd/src/models/user.py
+++ BackEnd/src/models/user.py
@@ -1,8 +1,17 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.dialects.postgresql import BYTEA
+from src.models.UserPassword import UserPassword
 from src.models.base import Base
 
 
 class User(Base):
+    def __init__(self, user_password: UserPassword):
+        self.password = user_password.password
+        self.salt = user_password.salt
+
+    def get_user_password(self):
+        return UserPassword(password=self.password, salt=self.salt)
+
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -10,3 +19,5 @@ class User(Base):
     phone = Column(String)
     email = Column(String)
     is_admin = Column(Boolean)
+    password = Column(BYTEA)
+    salt = Column(BYTEA)
