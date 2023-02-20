90393c5e90ace2e98610f37906ae89271e257d28
00XAH00 <pxaxulin@gmail.com>
2023-02-20 17:15:14 +0300

    add password settings

--- BackEnd/src/core/settings.py
+++ BackEnd/src/core/settings.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     host: str = 'localhost'
     port: int = 9000
     connection_string: str
+    password_hash_algorithm: str
+    password_encoding: str
 
 
 settings = Settings(
