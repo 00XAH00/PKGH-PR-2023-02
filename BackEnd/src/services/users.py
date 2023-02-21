f5bd594a28b47d7e1f17adbe2bd11cd73d1fe5c0
00XAH00 <pxaxulin@gmail.com>
2023-02-22 02:15:36 +0300

    add create user in user service

--- BackEnd/src/services/users.py
+++ BackEnd/src/services/users.py
@@ -1,15 +1,31 @@
 import hashlib
 import os
 from fastapi import Depends
-
 from src.core.settings import settings
 from src.db.db import Session, get_session
-from src.models.UserPassword import UserPassword
+from src.models.schemas.user_create import UserCreateSchema
+from src.models.user_password import UserPassword
+from src.models.user import User
 
 
 class UserService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
+
+    def create_user(self, user_create: UserCreateSchema) -> User:
+        password = self.password_hash(user_create.password)
+        user = User(
+            user_password=password,
+            first_name=user_create.first_name,
+            second_name=user_create.second_name,
+            phone=user_create.phone,
+            email=user_create.email
+        )
+
+        self.session.add(user)
+        self.session.commit()
+
+        return user
 
     @staticmethod
     def password_hash(password: str) -> UserPassword:
