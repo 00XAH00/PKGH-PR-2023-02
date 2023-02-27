9d1016ce15b870894b69e2a58efc7eb03fcaeef9
00XAH00 <pxaxulin@gmail.com>
2023-02-27 19:10:13 +0300

    add unique for goods name

--- BackEnd/src/models/goods.py
+++ BackEnd/src/models/goods.py
@@ -5,7 +5,7 @@ from src.models.base import Base
 class Product(Base):
     __tablename__ = "goods"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
     price = Column(Integer)
     code = Column(String(length=15))
     description = Column(JSON)
