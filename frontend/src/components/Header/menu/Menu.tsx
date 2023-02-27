a1c280fa25c329597c5157cbaea6fa0baf287b0d
Mountain-bear7 <doluxan010406@gmail.com>
2023-02-27 13:37:49 +0300

    implemented the menu

--- frontend/src/components/Header/menu/Menu.tsx
+++ frontend/src/components/Header/menu/Menu.tsx
@@ -6,7 +6,22 @@ const Menu: FC = () => {
 
     return(
         <div className={styles.menu}>
-            Menu
+            <a href="/">
+                <img src="../src/assets/logo.jpg"
+                     width={200}
+                     height={200}
+                     alt="Apple online store"/>
+            </a>
+
+            <nav>
+                <ul>
+                    <li><a href="/">Main</a></li>
+                    <li><a href="/">Mac</a></li>
+                    <li><a href="/">iPhone</a></li>
+                    <li><a href="/">Watch</a></li>
+                    <li><a href="/">AirPods</a></li>
+                </ul>
+            </nav>
         </div>
     )
 
