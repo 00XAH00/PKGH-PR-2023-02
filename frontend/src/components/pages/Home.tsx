cc858e821855329f5a03f8ea6d81084d0dbe3b3c
Mountain-bear7 <doluxan010406@gmail.com>
2023-02-27 13:20:22 +0300

    add a home page and header components

---
+++ frontend/src/components/pages/Home.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+// import styles from './Home.module.scss'
+import Header from "../Header/Header";
+
+const Home: FC = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+export default Home
