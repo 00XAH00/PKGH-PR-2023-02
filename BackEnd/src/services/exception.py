e3ada09649771a30bd0e1cbaabd2664f1c52a342
00XAH00 <pxaxulin@gmail.com>
2023-02-28 19:28:41 +0300

    add categories and manufacture in exception service

--- BackEnd/src/services/exception.py
+++ BackEnd/src/services/exception.py
@@ -21,8 +21,24 @@ class ExceptionService:
             error_details = "Данный пользователь уже существует"
         if exception_block_error == "product":
             error_details = "Данный товар уже существует"
+        if exception_block_error == "manufacture":
+            error_details = "Данный производитель уже существует"
+        if exception_block_error == "category":
+            error_details = "Данная категория уже существует"
 
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=error_details)
+
+    @staticmethod
+    def not_exist_error(exception_block_error: str):
+        error_details = "Информация не доступна"
+        if exception_block_error == "manufacture":
+            error_details = "Производитель не существует"
+        if exception_block_error == "user":
+            error_details = "Пользователь не существует"
+        if exception_block_error == "category":
+            error_details = "Данная категория не существует"
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=error_details)
 
     @staticmethod
     def token_error():
