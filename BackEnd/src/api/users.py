27968292c580647e819f0587d78c709be277b047
00XAH00 <pxaxulin@gmail.com>
2023-02-28 15:42:21 +0300

    add route for change user password and change response code in create user route

--- BackEnd/src/api/users.py
+++ BackEnd/src/api/users.py
@@ -3,7 +3,9 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from starlette import status
 from src.models.schemas.jwt import JWT
 from src.models.schemas.user_create import UserCreateSchema
+from src.models.schemas.user_password_change import UserPasswordChangeSchema
 from src.models.schemas.user_response import UserResponse
+from src.services.exception import ExceptionService
 from src.services.users import UserService
 
 
@@ -19,7 +21,8 @@ def get_current_user_id(token: str = Depends(oauth2_schema)) -> int:
     return UserService.verify_token(token)
 
 
-@router.post('/create_user', response_model=UserResponse, name="Добавление пользователя")
+@router.post('/create_user', response_model=UserResponse, name="Добавление пользователя",
+             status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreateSchema, user_service: UserService = Depends()):
     """
         Создание пользователя
@@ -36,8 +39,11 @@ def remove_user(remove_user_id: int, user_service: UserService = Depends(),
     return user_service.remove(removable_user_id=remove_user_id, main_user_id=main_user_id)
 
 
-@router.put('/change_user_password')
-def change_user_password(user_service: UserService = Depends()): ...
+@router.put('/change_user_password', name="Изменение пароля пользователя")
+def change_user_password(user_password_change: UserPasswordChangeSchema, user_service: UserService = Depends(),
+                         main_user_id: int = Depends(get_current_user_id)):
+
+    user_service.change_user_password(main_user_id=main_user_id, changeable_user_password=user_password_change)
 
 
 @router.post('/authorize', response_model=JWT, name="Авторизация пользователя")
