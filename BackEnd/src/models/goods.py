89742a89ee0569100e7ca4eba8e2571906626c1c
00XAH00 <pxaxulin@gmail.com>
2023-02-28 19:33:09 +0300

    add not null in goods table

--- BackEnd/src/models/goods.py
+++ BackEnd/src/models/goods.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, Boolean, JSON
+from sqlalchemy import Column, Integer, String, Boolean, JSON, ForeignKey
 from src.models.base import Base
 
 
 class Product(Base):
     __tablename__ = "goods"
     id = Column(Integer, primary_key=True)
-    manufacture_name = Column(String)
-    name = Column(String)
-    price = Column(Integer)
-    code = Column(String(length=15), unique=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    code = Column(String(length=15), unique=True, nullable=False),
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    manufacture_id = Column(Integer, ForeignKey('manufactures.id'), nullable=False)
     description = Column(JSON)
-    is_available = Column(Boolean)
+    is_available = Column(Boolean, nullable=False)
