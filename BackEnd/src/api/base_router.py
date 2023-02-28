772e9e4bef4102ebe41823ece697dc451da7089d
00XAH00 <pxaxulin@gmail.com>
2023-02-28 19:29:46 +0300

    add categories and manufacture routers

--- BackEnd/src/api/base_router.py
+++ BackEnd/src/api/base_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from src.api import users
+from src.api import users, goods, manufacture, category
 
 router = APIRouter()
 router.include_router(users.router)
+router.include_router(manufacture.router)
+router.include_router(category.router)
