a325585dba37fd113014e6884e875d78d2d79417
00XAH00 <pxaxulin@gmail.com>
2023-03-01 02:43:38 +0300

    add authorize on route for goods

--- BackEnd/src/api/goods.py
+++ BackEnd/src/api/goods.py
@@ -4,6 +4,7 @@ from starlette import status
 from src.models.schemas.product_change_price import ProductChangePriceSchema
 from src.models.schemas.product_create import ProductCreateSchema
 from src.models.schemas.product_response import ProductResponse
+from src.services.exception import ExceptionService
 from src.services.goods import ProductService
 from src.services.users import UserService
 
@@ -22,24 +23,41 @@ def get_current_user_id(token: str = Depends(oauth2_schema), user_service: UserS
 
 @router.post('/create_product', response_model=ProductResponse, name="Добавление товара",
              status_code=status.HTTP_201_CREATED)
-def create_product(product: ProductCreateSchema, product_service: ProductService = Depends()):
+def create_product(product: ProductCreateSchema, product_service: ProductService = Depends(),
+                   user_service: UserService = Depends(), user_id: int = Depends(get_current_user_id),
+                   exception_service: ExceptionService = Depends()):
     """
         Добавление товара
     """
+    if not user_service.is_user_admin(user_id):
+        exception_service.forbidden_error()
+
     return product_service.create_product(product)
 
 
 @router.delete('/remove_product', name="Удаление товара", status_code=status.HTTP_204_NO_CONTENT)
-def remove_product(product_code: str, product_service: ProductService = Depends()):
+def remove_product(product_code: str, product_service: ProductService = Depends(),
+                   user_service: UserService = Depends(), user_id: int = Depends(get_current_user_id),
+                   exception_service: ExceptionService = Depends()
+                   ):
     """
         Удаление товара
     """
+    if not user_service.is_user_admin(user_id):
+        exception_service.forbidden_error()
+
     return product_service.remove_product(product_code)
 
 
 @router.put('/change_product_price', name="Изменение цены товара")
-def remove_product(product_price: ProductChangePriceSchema, product_service: ProductService = Depends()):
+def remove_product(product_price: ProductChangePriceSchema, product_service: ProductService = Depends(),
+                   user_service: UserService = Depends(), user_id: int = Depends(get_current_user_id),
+                   exception_service: ExceptionService = Depends()
+                   ):
     """
         Изменение цены товара
     """
+    if not user_service.is_user_admin(user_id):
+        exception_service.forbidden_error()
+
     return product_service.change_product_price(product_price)
