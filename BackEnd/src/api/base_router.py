2f4aac0c1d03b81a8ab2eaa6d0222c264ae8ee68
00XAH00 <pxaxulin@gmail.com>
2023-03-01 12:29:59 +0300

    import cart object router

--- BackEnd/src/api/base_router.py
+++ BackEnd/src/api/base_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from src.api import users, goods, manufacture, category
+from src.api import users, goods, manufacture, category, cart_objects
 
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(goods.router)
 router.include_router(manufacture.router)
 router.include_router(category.router)
+router.include_router(cart_objects.router)
