7787ce68c742b580b7cb7d61c9a2c9d54355a102
00XAH00 <pxaxulin@gmail.com>
2023-03-02 01:26:19 +0300

    add route for product picture

--- BackEnd/src/api/base_router.py
+++ BackEnd/src/api/base_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.api import users, goods, manufacture, category, cart_objects
+from src.api import users, goods, manufacture, category, cart_objects, picture
 
 router = APIRouter()
 router.include_router(users.router)
@@ -7,3 +7,4 @@ router.include_router(goods.router)
 router.include_router(manufacture.router)
 router.include_router(category.router)
 router.include_router(cart_objects.router)
+router.include_router(picture.router)
