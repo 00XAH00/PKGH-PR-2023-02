5ee1608d49882844996d1de502bee9e0c630c346
00XAH00 <pxaxulin@gmail.com>
2023-03-02 01:30:44 +0300

    add root_path param

--- BackEnd/src/main.py
+++ BackEnd/src/main.py
@@ -2,16 +2,10 @@ import uvicorn
 from src.core.settings import settings
 
 if __name__ == "__main__":
-    if settings.host:
-        uvicorn.run(
-            "app:app",
-            host=settings.host,
-            port=settings.port,
-            reload=True
-        )
-    else:
-        uvicorn.run(
-            "app:app",
-            port=settings.port,
-            reload=True
-        )
+    uvicorn.run(
+        "app:app",
+        host=settings.host,
+        port=settings.port,
+        root_path="./",
+        reload=True
+    )
