efb350290649c33efd7a10f64bf18a3915b19201
Mountain-bear7 <doluxan010406@gmail.com>
2023-03-02 16:48:44 +0300

    add redux in main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
 import Home from "./pages/Home";
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <ChakraProvider>
-        <Home/>
-    </ChakraProvider>
+    <Provider store={store}>
+        <ChakraProvider>
+            <Home/>
+        </ChakraProvider>
+    </Provider>
 )
