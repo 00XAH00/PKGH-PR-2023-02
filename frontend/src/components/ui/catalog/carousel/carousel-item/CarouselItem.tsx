380cb9d3c1081a175ded388dfa877eb8c40bec6d
Mountain-bear7 <doluxan010406@gmail.com>
2023-03-03 12:30:43 +0300

    wrote the basic structure of the product catalog

--- frontend/src/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
+++ frontend/src/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
@@ -1,14 +1,25 @@
 import {FC} from "react";
 import {IProduct} from "../../../../../types/product.interface";
 import styles from './Carousel.module.scss'
+import cn from "clsx";
+
+const isActive = false//временная заглушка
 
 const CarouselItem: FC<{product: IProduct}> = ({product}) => {
     return(
-        <div>
-            <img alt={product.name} src={product.images[0]} width={200} height={200}/>
+        <div className={cn(styles.item, {
+            [styles.active]: isActive
+        })}>
 
-            <div className={styles.heading}></div>
+            <img className={styles.image} alt={product.name} src={product.images[0]} width={200} height={200}/>
 
+            <div className={styles.heading}>
+                {product.name}
+            </div>
+
+            <div className={styles.description}>
+                {product.description}
+            </div>
 
         </div>
     )
